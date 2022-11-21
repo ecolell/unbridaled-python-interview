e9748d742374261dd2d155de31f335977f23e245
eloy <eloy.colell@gmail.com>
2022-11-21 09:50:48 -0300

    Update ProductCreate and ProductVariantCreact pydantic schemas.

--- store/app/routers/v1/schemas.py
+++ store/app/routers/v1/schemas.py
@@ -1,12 +1,21 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from app.models.product import ProductType, UOMType
+from app.models.product_variant import ProductVariantType
 from pydantic import BaseModel
 
 
 class ProductVariantCreate(BaseModel):
-    pass
+    id: int
+    sku: str
+    sales_price: int
+    product_id: int
+    purchase_price: int
+    type: ProductVariantType
+    created_at: datetime
+    updated_at: datetime
+    config_attributes: List
 
 
 class ProductCreate(BaseModel):
@@ -16,8 +25,9 @@ class ProductCreate(BaseModel):
     is_producible: bool
     is_purchasable: bool
     type: ProductType
-    purchase_uom: UOMType
-    purchase_uom_conversion_rate: int
+    purchase_uom: Optional[UOMType]
+    purchase_uom_conversion_rate: Optional[int]
+    batch_tracked: bool
     additional_info: str
     created_at: datetime
     updated_at: datetime
