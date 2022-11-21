efb7d958128acae0d6cbf40f8df3c529c764ffa5
eloy <eloy.colell@gmail.com>
2022-11-21 02:00:54 -0300

    Remove not used import.

--- store/app/db.py
+++ store/app/db.py
@@ -1,7 +1,5 @@
 from typing import AsyncGenerator
 
-from sqlmodel import SQLModel
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
