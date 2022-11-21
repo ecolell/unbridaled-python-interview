b4bfaaf374d313282ecbecba747d346c76a72914
eloy <eloy.colell@gmail.com>
2022-11-21 01:56:32 -0300

    Declare get_session generator to generate a database session on each fastapi request.

---
+++ store/app/db.py
@@ -0,0 +1,18 @@
+from typing import AsyncGenerator
+
+from sqlmodel import SQLModel
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+
+
+engine = create_async_engine(settings.database_url, echo=True, future=True)
+
+
+async def get_session() -> AsyncGenerator:
+    async_session = sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with async_session() as session:
+        yield session
