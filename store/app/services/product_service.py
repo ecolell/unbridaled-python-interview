ba252003e94eb747b87317367413b3f2c4680118
eloy <eloy.colell@gmail.com>
2022-11-23 02:19:46 -0300

    Create ProductVariant objects. Rollback if required.

--- store/app/services/product_service.py
+++ store/app/services/product_service.py
@@ -1,9 +1,12 @@
 from typing import TYPE_CHECKING
-from sqlalchemy.ext.asyncio import AsyncSession
+
+import sqlalchemy as sa
 from app.models.product import Product
+from app.models.product_variant import ProductVariant
+from sqlalchemy.ext.asyncio import AsyncSession
 
 if TYPE_CHECKING:
-    from app.routers.v1 import ProductCreate
+    from app.routers.v1.schemas import ProductCreate
 
 
 class ProductError(Exception):
@@ -11,8 +14,20 @@ class ProductError(Exception):
 
 
 async def create_product(session: AsyncSession, data: "ProductCreate") -> "Product":
-    product = Product(**data.__dict__)
+    product_data = data.dict(exclude={"variants"})
+    product = Product(**product_data)
     session.add(product)
-    await session.commit()
-    await session.refresh(product)
+    variants = []
+    for data_variant in data.variants:
+        product_variant_data = data_variant.dict()
+        variant = ProductVariant(**product_variant_data)
+        variants.append(variant)
+        session.add(variant)
+    product.variants = variants
+    try:
+        await session.commit()
+        await session.refresh(product)
+    except sa.exc.IntegrityError:
+        await session.rollback()
+        raise ProductError()
     return product
