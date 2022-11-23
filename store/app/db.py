f2c03c832ff18ba3f80dd0897d06abda6e3e4ca8
eloy <eloy.colell@gmail.com>
2022-11-23 17:54:08 -0300

    Apply isort and black for code consistency.

--- store/app/db.py
+++ store/app/db.py
@@ -1,16 +1,13 @@
 from typing import AsyncGenerator
 
+from app.config import settings
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from app.config import settings
-
 
 engine = create_async_engine(settings.async_database_url, echo=True, future=True)
 
 
 async def get_session() -> AsyncGenerator:
-    async_session = sessionmaker(
-        engine, class_=AsyncSession, expire_on_commit=False
-    )
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
         yield session
